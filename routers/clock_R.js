5a7f8a8aa36c5aee38bec95913f8fe78856be54f
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-09-26 01:00:17 +0300

    Update clock_R.js
    added routers for posting stamps table & getting stamps table

--- routers/clock_R.js
+++ routers/clock_R.js
@@ -11,11 +11,8 @@ router.get("/",(req, res) => {
 router.post("/Add",(req, res) => {
 
     let ThemesObj = {};
-    //ThemesObj.category  = req.body.category;
-    //ThemesObj.task_name = req.body.task_name;
     ThemesObj.first_name = req.body.first_name;
     ThemesObj.last_name  = req.body.last_name;
-
 
     const addQuery = `INSERT INTO employee_id(first_name,last_name) VALUES('${ThemesObj.first_name}','${ThemesObj.last_name}')`;
     db_pool.query(addQuery,function (err,rows,fields,){
@@ -28,6 +25,34 @@ router.post("/Add",(req, res) => {
     console.log(ThemesObj);
 });
 
+router.post("/AddEnterTime",(req,res)=>{
+    let employee_id = req.body.employee_id;
+
+    const addQuery = `INSERT INTO time_stamps(date , employee_id , enter_time) VALUES(CURRENT_DATE , '${employee_id}' , CURRENT_TIME)`;
+
+    db_pool.query(addQuery,function (err,rows){
+        if (err){
+            res.status(500).json({message:err});
+        }
+        else {
+            res.status(200).json({message: "enter stamp added"});
+        }
+    })
+});
+router.patch("/AddExitTime",(req,res)=>{
+    let employee_id = req.body.employee_id;
+
+    const addQuery = `UPDATE time_stamps SET exit_time = CURRENT_TIME WHERE employee_id = '${employee_id}' AND date = CURRENT_DATE`;
+
+    db_pool.query(addQuery,function (err,rows){
+        if (err){
+            res.status(500).json({message:err});
+        }
+        else {
+            res.status(200).json({message: "exit stamp added"});
+        }
+    })
+});
 router.patch("/Edit",(req, res) => {
 
     let newAnswerObj = {};
@@ -84,5 +109,24 @@ router.get("/List",(req, res) => {
 
         }
     });
+});
+router.post("/Employee",(req, res) => {
 
+    let stamp_id = req.body.stamp_id;
+
+    let q=`SELECT * FROM time_stamps WHERE employee_id= '${stamp_id}'`;
+
+    db_pool.query(q, function(err, rows, fields){
+
+        if(err)
+        {
+            res.status(500).json({message: err})
+
+        }
+        else
+        {
+            res.status(200).json(rows);
+
+        }
+    });
 });
