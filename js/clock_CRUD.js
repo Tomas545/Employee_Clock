38bae6a250800874159f59a5659a0bc4fb57bb91
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-09-26 01:02:56 +0300

    Update clock_CRUD.js
    enter and exit + create table for employee stamps

--- js/clock_CRUD.js
+++ js/clock_CRUD.js
@@ -1,18 +1,46 @@
 let raw_data=[];
+let employee_data = [];
+let srchTerm="";
 
-
+function FilterData(el){
+    console.log("FilterData::",el);
+    if(srchTerm==="")
+        return true;
+    let reg=new RegExp(srchTerm,"i");
+    if(reg.test(el.last_name)){
+        return true;
+    }
+    return false;
+}
 function CreateTble(){
+    srchTerm=document.getElementById("filterField").value;
+    let data=raw_data.filter(FilterData);
     let str="";
-    for(let line of raw_data){
+    for(let line of data){
         str+="<tr>";
         str+=`<td><button onclick="editLine(${line.id});">edit employee</button></td>`;
         str+="<td>"+line.id+"</td>";
         str+="<td>"+line.first_name+"</td>";
         str+="<td>"+line.last_name+"</td>";
         str+=`<td><button onclick="deleteLine(${line.id});">delete employee</button></td>`;
+        str+=`<td><button onclick="selectLine(${line.id});">select employee</button></td>`;
         str+="</tr>";
     }
-    document.getElementById("mainTable").innerHTML=str;
+    document.getElementById("employeeTable").innerHTML=str;
+}
+function CreateStampTable(){
+
+    let data=employee_data;
+    let str="";
+    for(let line of data){
+        str+="<tr>";
+        str+="<td>"+line.date+"</td>";
+        str+="<td>"+line.employee_id+"</td>";
+        str+="<td>"+line.enter_time+"</td>";
+        str+="<td>"+line.exit_time+"</td>";
+        str+="</tr>";
+    }
+    document.getElementById("stamping_table").innerHTML=str;
 }
 
 async function getList() {
@@ -22,6 +50,22 @@ async function getList() {
     console.log("data=",data);
     raw_data = data;
     CreateTble();
+}
+async function getStamps() {
+
+    let stamp_id = document.getElementById("stamp_id").value;
+    let response = await fetch('/Employee', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({stamp_id:stamp_id})
+        }
+    );
+    let data = await response.json();
+    console.log("data=",data);
+    employee_data = data;
+    CreateStampTable();
 }
 
 async function addNewLine() {
@@ -55,6 +99,11 @@ async function deleteLine(id) {
 // console.log(data);
     getList();
 }
+
+async function selectLine(id){
+    let objToServer={};
+    objToServer.idx=id;
+}
 async function editLine(id) {
     let objToServer={};
     objToServer.idx=id;
@@ -71,4 +120,32 @@ async function editLine(id) {
     );
     getList();
 }
+async function addEnterTime(){
+    let employee_id=document.getElementById("employee_id").value;
+
+    let response = await fetch('/AddEnterTime', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({employee_id: employee_id})
+        }
+    );
+}
+
+async function addExitTime(){
+    let employee_id=document.getElementById("employee_id").value;
+
+    let response = await fetch('/AddExitTime', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({employee_id: employee_id})
+        }
+    );
+}
+
 getList();
+getStamps();
+
