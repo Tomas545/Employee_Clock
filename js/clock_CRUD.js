db1fe0d5ccb3049651ee45ab07978f43d9f91404
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-09-27 19:56:55 +0300

    Update clock_CRUD.js

--- js/clock_CRUD.js
+++ js/clock_CRUD.js
@@ -1,7 +1,5 @@
 let raw_data=[];
-//let employee_data = [];
 let srchTerm="";
-//let employeeSelected = false;
 
 function FilterData(el){
     console.log("FilterData::",el);
@@ -30,49 +28,7 @@ function CreateTble(){
     document.getElementById("employeeTable").innerHTML=str;
 
 }
-/*
-function CreateTble2(){
-    srchTerm=document.getElementById("filterField2").value;
-    let data=raw_data.filter(FilterData);
-    let str="";
-    for(let line of data){
-        str+="<tr>";
-        str+="<td>"+line.id+"</td>";
-        str+="<td>"+line.first_name+"</td>";
-        str+="<td>"+line.last_name+"</td>";
-        str+=`<td><button onclick="selectLine(${line.id});">select employee</button></td>`;
-        str+="</tr>";
-    }
-    document.getElementById("employeeTable2").innerHTML=str;
 
-}
-*/
-/*
-function CreateStampTable(){
-//line.date.toString().substring(0,10)
-    let data=employee_data;
-    let str="";
-    for(let line of data){
-        str+="<tr>";
-        str+="<td>"+reverseString(line.date.toString())+"</td>";
-        str+="<td>"+line.employee_id+"</td>";
-        str+="<td>"+line.enter_time+"</td>";
-        str+="<td>"+line.exit_time+"</td>";
-        str+="</tr>";
-    }
-    document.getElementById("stamping_table").innerHTML=str;
-    //document.getElementById("stamping_table2").innerHTML=str;
-
-}*/
-/*
-function reverseString(str) {
-    str = str.substring(0,10);
-    let str2 = "";
-    str2 += str.substring(8);
-    str2 += str.substring(4,8);
-    str2 += str.substring(0,4);
-    return str2;
-}*/
 async function getList() {
     let response = await fetch('/List');
 // console.log("response=",response);
@@ -82,34 +38,7 @@ async function getList() {
     CreateTble();
 
 }
-/*
-async function getList2() {
-    let response = await fetch('/List');
-// console.log("response=",response);
-    let data = await response.json();
-    console.log("data=",data);
-    raw_data = data;
-    CreateTble2();
-}
-*/
-/*
-async function getStamps() {
 
-    let stamp_id = document.getElementById("employee_id").value;
-    let response = await fetch('/Employee', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({stamp_id:stamp_id})
-        }
-    );
-    let data = await response.json();
-    console.log("data=",data);
-    employee_data = data;
-    CreateStampTable();
-}
-*/
 async function addNewLine() {
     let first_name=document.getElementById("first_name").value;
     let last_name=document.getElementById("last_name").value;
@@ -137,16 +66,10 @@ async function deleteLine(id) {
             body: JSON.stringify(objToServer)
         }
     );
-// let data = await response.json();
-// console.log(data);
+
     getList();
 }
-/*
-async function selectLine(id){
-    employeeSelected = true;
-    document.getElementById("employee_id").value = id;
-    getStamps();
-}*/
+
 
 async function editLine(id) {
     let objToServer={};
@@ -164,39 +87,5 @@ async function editLine(id) {
     );
     getList();
 }
-/*
-async function addEnterTime(){
-    if (employeeSelected === true){
-    let employee_id=document.getElementById("employee_id").value;
-    let response = await fetch('/AddEnterTime', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({employee_id: employee_id})
-        }
-    );
-    //getStamps();
-    employeeSelected = false;
-    }
-}
-
-async function addExitTime(){
-    let employee_id=document.getElementById("employee_id").value;
-
-    let response = await fetch('/AddExitTime', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({employee_id: employee_id})
-        }
-    );
-    //getStamps();
-}
-*/
 
 getList();
-//getList2()
-//getStamps();
-//document.getElementById("employee_id").value = " ";
